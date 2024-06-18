b03c673514d25b25c94d7a5495eb0b26db5705f4
QuentinVigier <vigierquentinpro@gmail.com>
2024-06-18 11:59:57 +0200

    Ajout du mode DAY/NIGHT avec dark mode et toggle de choix

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ async function setupCamera() {
         video.play();
     };
 }
+
+//SWITCH DU MODE JOUR/NUIT AU CHANGEMENT DU TOGGLE
+document.addEventListener('DOMContentLoaded', () => {
+    const modeSwitch = document.getElementById('mode-switch');
+    const body = document.body;
+
+    modeSwitch.addEventListener('change', () => {
+        if (modeSwitch.checked) {
+            body.classList.remove('day-mode');
+            body.classList.add('night-mode');
+        } else {
+            body.classList.remove('night-mode');
+            body.classList.add('day-mode');
+        }
+    });
+});
