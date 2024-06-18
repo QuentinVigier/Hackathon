5ef6a377e44822be6320e6572eb299b542d17ae6
migmar basara <mdolma@ymail.com>
2024-06-18 09:31:13 +0200

    affichage web cam dans browser

---
+++ index.js
@@ -0,0 +1,12 @@
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', setupCamera);
+
+
+
+async function setupCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    video.onloadedmetadata = () => {
+        video.play();
+    };
+}
