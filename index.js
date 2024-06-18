bc61f283bc6764ffd72c5313a002019c909f976b
bestdevelope <leabouillot7@gmail.com>
2024-06-18 13:00:06 +0200

    COCO-SSD et dectection objet

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let video = document.getElementById('video');
 let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');
 let captureButton = document.getElementById('capture');
-
+let predictionsElement = document.getElementById('predictions');
 
 const startButton = document.getElementById('startButton');
 startButton.addEventListener('click', setupCamera);
@@ -28,9 +28,36 @@ async function getConnectedDevices(type) {
 const videoCameras = getConnectedDevices('videoinput');
 console.log('Cameras found:', videoCameras);
 
+//Chargement du modele COCO-SSD
+let model;
+async function loadModel() {
+    model = await cocoSsd.load();
+    console.log("Modèle COCO-SSD chargé.");
+}
+loadModel();
+
 //Léa:  Capture d'image
 captureButton.addEventListener('click', () => {
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     canvas.style.display = 'block';
     detectObjects();
 });
+
+// Dectection objet
+async function detectObjects() {
+    const predictions = await model.detect(canvas);
+    predictionsElement.innerHTML = '';
+    predictions.forEach((prediction) => {
+        // Pour chaque prediction, je commence un nouveau tracé
+        ctx.beginPath();
+        ctx.rect(...prediction.bbox)
+        ctx.lineWidth = 2;
+        ctx.strokeStyle ='red';
+        ctx.fillStyle ='red';
+        ctx.stroke();
+        ctx.fillText(
+            `${prediction.class}`, prediction.bbox[0], prediction.bbox[1]);    
+})
+}
+
+
